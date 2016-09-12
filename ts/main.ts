3a6d98efd2b02490605bb85718a23541a91aa02c
AlexD <alxdmr2@yahoo.fr>
2016-09-12 22:31:35 +0200

    init V2

--- ts/main.ts
+++ ts/main.ts
@@ -1,59 +1,6 @@
-/***********************************************************************************************************************
- *
- */
-console.log( "Au chargement du script, document.body = ", document.body );
+import { miniJQ } from "./miniJQ";
 
-
-/***********************************************************************************************************************
- * Q1) Affichez la référence à document.body APRES que le document soit chargé.
- * Inspirez vous du TP1 pour trouver comment faire.
- */
-
-
-/***********************************************************************************************************************
- * Q2) Ecrivez la fonction miniJQ :
- *   - argument : un sélecteur CSS ou un Element du DOM
- *   - renvoi une instance de classe MiniJQData
- */
-function miniJQ( selecteur: string | HTMLElement | HTMLElement[] ) : MiniJQData {
-    let elements = [];
-    if (selecteur instanceof String) {
-        console.log( selecteur, "is a string" );
-    }
-    if (selecteur instanceof HTMLElement) {
-        console.log( selecteur, "is a HTMLElement" );
-    }
-    if (selecteur instanceof Array) {
-        console.log( selecteur, "is an array of HTMLElements" );
-    }
-    return new MiniJQData( [] ); // A modifier bien sur...
-}
 
 miniJQ( "p" );
-setTimeout( () => miniJQ( document.body ), 1000 );
-setTimeout( () => miniJQ( [document.body] ), 1500 );
-
-/***********************************************************************************************************************
- * Q3) Ecrivez la classe MiniJQData de sorte à mimer le comportement de JQuery
- * voir http://api.jquery.com/
- * * Au minimum, les méthodes :
- *   - each
- *   - text
- *   - html
- *   - click
- *   - append
- *   - bind
- *   - children
- *   - detach
- */
-class MiniJQData {
-    readonly elements    : HTMLElement[];
-    constructor( public elements: HTMLElement[] ) {
-        this.elements = elements;
-    }
-}
-
-
-/***********************************************************************************************************************
- * Q3) Etendez la classe MiniJQData avec d'autres méthodes de votre choix
- */
+miniJQ( document.body );
+miniJQ( [document.body] );
---
+++ ts/miniJQ.ts
@@ -0,0 +1,45 @@
+/***********************************************************************************************************************
+ * Q1) Ecrivez la classe MiniJQData de sorte à mimer le comportement de JQuery
+ * voir http://api.jquery.com/
+ * * Au minimum, les méthodes :
+ *   - each
+ *   - text
+ *   - html
+ *   - click
+ *   - append
+ *   - bind
+ *   - children
+ *   - detach
+ */
+class MiniJQData {
+    readonly bob        : string = "bob";
+    private elements    : HTMLElement[];
+    constructor( elements: HTMLElement[] ) {
+        this.elements = elements;
+    }
+}
+
+/***********************************************************************************************************************
+ * Q2) Ecrivez la fonction miniJQ :
+ *   - argument : un sélecteur CSS ou un Element du DOM
+ *   - renvoi une instance de classe MiniJQData
+ */
+export let miniJQ = ( selecteur: string | HTMLElement | HTMLElement[] ) : MiniJQData => {
+    let elements = [];
+    if (selecteur.constructor === String) {
+        console.log( selecteur, "is a string" );
+    }
+    if (selecteur instanceof HTMLElement) {
+        console.log( selecteur, "is a HTMLElement" );
+    }
+    if (selecteur instanceof Array) {
+        console.log( selecteur, "is an array of HTMLElements" );
+    }
+    return new MiniJQData( elements ); // A modifier bien sur...
+};
+
+
+/***********************************************************************************************************************
+ * Q3) Etendez la classe MiniJQData avec d'autres méthodes de votre choix
+ *  Par exemple des méthodes issues de JQueryUI
+ */
